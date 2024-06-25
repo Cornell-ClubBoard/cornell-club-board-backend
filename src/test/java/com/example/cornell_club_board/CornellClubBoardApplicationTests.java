d1b1712b492d3e19d55347d85fb433b1267205cc
DylanVig <drv36@cornell.edu>
2024-06-24 22:08:57 -0400

    Initial Commit

---
+++ src/test/java/com/example/cornell_club_board/CornellClubBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cornell_club_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CornellClubBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
